794903165f4c7d287688efd47ada22f20dce436d
Bilkis <bikisismail07@Gmail.com>
2022-04-14 08:14:04 +0700

    update

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: process.env.DB_TYPE,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_TYPE,
   },
 };
